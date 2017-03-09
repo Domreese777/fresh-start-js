864a6fe95684e5974bd4580c8af594bd317653d1
Domreese777 <dar29@students.uwf.edu>
2017-03-08 20:43:29 -0600

    Using fetch to make http request

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -28,6 +28,14 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req,res){
+  res.json([
+    {"id": 1, "firstName":"Bob","lastName":"Smith","email":"dod@gmail.com"},
+    {"id": 1, "firstName":"Charles","lastName":"Reed","email":"creed@gmail.com"},
+    {"id": 1, "firstName":"Alanis","lastName":"Reese","email":"areese@gmail.com"}
+  ]);
+});
+
 app.listen(port,function(err){
   if (err){
     console.log(err);
